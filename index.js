54ee237782a2bf0de593d2d6ddae5a168d83873a
tonythebrother <ronnyyoelperez@gmail.com>
2020-11-29 23:44:58 -0400

    Finish Conections to Broker on localhost

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const mqttPub = () => require('./publish');
-const mqttSub = () => require('./subscribe');
 const host = require('ip').address();
 
 //valor en segundos
@@ -7,15 +5,16 @@ const red = 15;
 const green = 10;
 const yellow = 5;
 
-var timeLeft;
 var colorLight;
+let timeLeftR;
 
 const topicLtrafic = 'avenue/light-trafic';
 const topicBtrafic = 'avenue/app-pedestrian';
 
 const traficLight = () => {
 
-    let timeLeftR = red;
+    let timeLeft;
+    timeLeftR = red;
     let timeLeftG = green;
     let timeLeftY = yellow;
 
@@ -35,12 +34,57 @@ const traficLight = () => {
         }
 
         process.stdout.write('\033c');
-        const countDown = `${colorLight}: Time Remaining ${timeLeft}`;
+        const countDown = `${colorLight}: Time Remaining ${timeLeft}s`;
         console.log(countDown);
         mqttPub(topicLtrafic, countDown, host);
-        //mqttSub(topicBtrafic, host);
+        
+
+        
 
     }, 1000);
 };
 
+const mqttPub = (topic, msg, host) => {
+
+    const mqtt = require('mqtt');
+    const client = mqtt.connect(`tcp://${host}:1883`);
+
+    client.on('connect', () => {
+        client.subscribe(topic, (err) => {
+            if (!err) {
+                client.publish(topic, msg);
+            }
+        });
+    });
+
+    client.on('message', (topic, message) => {
+        console.log("Was send successfully.");
+        client.end();
+    });
+    
+}
+
+ const mqttSub = (topic, host) => {
+    const mqtt = require('mqtt');
+    const client = mqtt.connect(`tcp://${host}:1883`);
+
+    client.on('message', (topic, message) => {
+
+        if(colorLight === "Red" && message == "true" && timeLeftR <= 15) {
+            if(timeLeftR < 10 && timeLeftR > 5) timeLeftR += 15;
+            else timeLeftR += 10;
+
+        }           
+        
+        console.log(`Message from ${topic}: ${message}`);
+        
+    });
+
+    client.on('connect', () => {
+        client.subscribe(topic);
+    });
+
+}
+
 traficLight();
+mqttSub(topicBtrafic, host);
